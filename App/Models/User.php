3318540ca31a649f6c708354c2109395f44ebc5e
alex <mr.kamc@gmail.com>
2016-06-05 22:39:07 +0300

    + ::findAll

--- App/Models/User.php
+++ App/Models/User.php
@@ -3,9 +3,19 @@
 namespace App\Models;
 
 
+use App\Db;
+
 class User
 {
     public $email;
     public $name;
 
+    public static function findAll()
+    {
+        $db = new Db();
+        return $db->query(
+            'SELECT * FROM users',
+            'App\Models\User'
+        );
+    }
 }
--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 
 require __DIR__ . '/autoload.php';
 
-$db = new \App\Db();
-$res = $db->query('SELECT * FROM users', 'App\\Models\\User');
-var_dump($res);
+$users = \App\Models\User::findAll();
+
+var_dump($users);
